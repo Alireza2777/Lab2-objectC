5941f50ff100e1694a407a9d5a784103b75b3363
Ali <alirezamonfared813@gmail.com>
2023-09-01 13:24:22 -0700

    sorry for late

---
+++ Lab 2/Box.h
@@ -0,0 +1,12 @@
+//
+//  Box.h
+//  Lab 2
+//
+//  Created by Alireza Karimi on 2023-09-01.
+//
+
+#ifndef Box_h
+#define Box_h
+
+
+#endif /* Box_h */
---
+++ Lab 2/Box.m
@@ -0,0 +1,8 @@
+//
+//  Box.m
+//  Lab 2
+//
+//  Created by Alireza Karimi on 2023-09-01.
+//
+
+#import <Foundation/Foundation.h>
